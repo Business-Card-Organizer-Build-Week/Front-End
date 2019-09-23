b90fdf0177946a4a2e07c25632139d7e7bf68f41
BNMoyers <50836425+BNMoyers@users.noreply.github.com>
2019-09-23 15:29:38 -0500

    Unit 2 Day 1 MVP: properly routed signup and login. Needs to be wired up to send/receive data from back-end

--- src/components/SignupLoginForm.js
+++ src/components/SignupLoginForm.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { withFormik, Form, Field, value } from 'formik';
 import * as yup from 'yup';
 import axios from 'axios';
