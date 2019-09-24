559d320e2cab98e4f0cb0c1ba976c74a7b911778
BNMoyers <50836425+BNMoyers@users.noreply.github.com>
2019-09-24 14:46:44 -0500

    set up card collection page with fake data, mapped to cards

--- src/components/CardCollection.js
+++ src/components/CardCollection.js
@@ -1,9 +1,51 @@
-import React from 'react'
+import React from 'react';
+import CardStyle2 from './CardStyle2';
+
+const fakecontacts = [
+    {
+        "username": "admin",
+        "fname": "John",
+        "lname": "Smith",
+        "busname": "TestBusName",
+        "title": "Title",
+        "contactid": 8,
+        "useremail": "test@test.com",
+        "userphone": "5555555555",
+        "useraddress": "TestAddress",
+        "usercity": "Test City",
+        "userState": "ST",
+        "userzip": "55555",
+        "usercontacttype": {
+            "contacttypeid": 4,
+            "contacttype": "Business"
+        }
+    },
+    {
+        "username": "admin",
+        "fname": "Jane",
+        "lname": "Doe",
+        "busname": "TestBusName",
+        "title": "Title",
+        "contactid": 9,
+        "useremail": "test@test.com",
+        "userphone": "5555555555",
+        "useraddress": "TestAddress",
+        "usercity": "Test City",
+        "userState": "ST",
+        "userzip": "55555",
+        "usercontacttype": {
+            "contacttypeid": 5,
+            "contacttype": "Home"
+        }
+    }
+
+];
 
 const CardCollection = () => {
     return(
         <div>
-            will map over a user's collected cards and display them using card components here.
+           {fakecontacts.map(contact => (
+               <CardStyle2 data={contact}/>))}
         </div>
     )
 }
